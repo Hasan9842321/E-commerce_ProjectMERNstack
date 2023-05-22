e1f88b63edf2cdd0fa457e243fce0cb0286e22cc
Md. Hasibul Hasan <abeer.rocky@gmail.com>
2023-05-22 12:33:54 +0600

    first

---
+++ server/src/app.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+// http-error hanland 
+const createError = require('http-errors');
+//API security 
+const xssClean = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+
+
+const rateLimitr = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    // standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    // legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    message: 'Too many requests from this IP. Please try again later'
+})
+
+
+app.use(xssClean());
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.status(200).send({
+        message: 'welcome to server'
+    });
+})
+
+app.get("/test", (req, res) => {
+    res.status(200).send({
+        message: 'GET: api testing done'
+    });
+})
+
+app.post("/test", (req, res) => {
+    res.status(200).send({
+        message: 'post: api testing done'
+    });
+})
+
+app.put("/test", (req, res) => {
+    res.status(200).send({
+        message: 'put: api testing done'
+    });
+})
+
+app.delete("/test", (req, res) => {
+    res.status(200).send({
+        message: 'delete: api testing done'
+    });
+})
+
+//client error handling 
+app.use((req, res, next) => {
+
+    next(createError(404, 'route not found'));
+})
+
+//server error handling 
+app.use((err, req, res, next) => {
+    return res.status(err.status || 500).json({
+        success: false,
+        message: err.message
+    });
+})
+
+
+module.exports = app;
