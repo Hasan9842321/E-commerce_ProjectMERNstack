59c09dfbd6cb5fbf7377a8c96a0b82551703e1b1
Md. Hasibul Hasan <abeer.rocky@gmail.com>
2023-05-23 12:40:43 +0600

    DataBase connected

--- server/src/secret.js
+++ server/src/secret.js
@@ -2,4 +2,6 @@ require('dotenv').config();
 
 const serverPort = process.env.SERVER_PORT || 500;
 
-module.exports = serverPort;
+const mongoDbUrll = (process.env.MONGODB_ATLAS_URL || 'mongodb://localhost:27017/ecommerceMernBD');
+
+module.exports = { serverPort, mongoDbUrll };
--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,16 @@
+const { connect } = require('mongoose');
 const app = require('./app');
-const serverPort = require('./secret');
+const { serverPort } = require('./secret');
+// const connectDb = require('./config/db');
+const connectDatabase = require('./config/db');
+// const serverPort = require('./secret');
+// const connectDb = require('./config/db');
 
-app.listen(serverPort, () => {
-    console.log(`server is tunning at http://localhost:${serverPort}`)
-})
+app.listen(serverPort, async() => {
+
+    console.log(`server is tunning at http://localhost:${serverPort}`);
+    await connectDatabase();
+
+
+
+});
