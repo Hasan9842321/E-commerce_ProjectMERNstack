67723c4bdec74cc2018b47d67a3d305032e2cb2f
Md. Hasibul Hasan <abeer.rocky@gmail.com>
2023-05-23 22:46:42 +0600

    user schema and model added

--- server/src/secret.js
+++ server/src/secret.js
@@ -4,4 +4,6 @@ const serverPort = process.env.SERVER_PORT || 500;
 
 const mongoDbUrll = (process.env.MONGODB_ATLAS_URL || 'mongodb://localhost:27017/ecommerceMernBD');
 
-module.exports = { serverPort, mongoDbUrll };
+const defaultImagePath = process.env.DEFAULT_IMAGE_PATH || 'public/images/users/men.png';
+
+module.exports = { serverPort, mongoDbUrll, defaultImagePath };
