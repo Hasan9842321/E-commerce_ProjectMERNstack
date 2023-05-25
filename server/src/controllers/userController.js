f45795b06cefb3ea37110afafd451ec1f4de5592
Md. Hasibul Hasan <abeer.rocky@gmail.com>
2023-05-25 21:42:10 +0600

    searching and pagination are done

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,11 +1,43 @@
 // http-error hanland 
 const createError = require('http-errors');
+const User = require('../models/userModel');
 
 
-const getUesrs = (req, res, next) => {
+const getUesrs = async(req, res, next) => {
     try {
+        const search = req.query.search || "";
+        const page = Number(req.query.page) || 1;
+        const limit = Number(req.query.limit) || 1;
+
+        const searchRegExp = new RegExp('.*' + search + '.*', 'i');
+
+        const filter = {
+            isAdmin: { $ne: true },
+            $or: [
+                { name: { $regex: searchRegExp } },
+                { email: { $regex: searchRegExp } },
+                { phone: { $regex: searchRegExp } },
+            ]
+        };
+
+        const options = { password: 0 }
+
+        const users = await User.find(filter, options).limit(limit)
+            .skip((page - 1) * limit);
+
+        const count = await User.find(filter).countDocuments();
+
+        //if no users found 
+        if (!users) throw createError(404, "no users found");
+
+
         res.status(200).send({
-            message: 'welcome to server'
+            message: 'users are returned',
+            users,
+            pagination: Math.ceil(count / limit),
+            currentPage: page,
+            previousPage: page - 1 > 0 ? page - 1 : null,
+            nextPage: page + 1 <= Math.ceil(count / limit) ? page + 1 : null
         });
 
     } catch (error) {
