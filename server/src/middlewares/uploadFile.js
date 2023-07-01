b5b67aa66de6ab261a9a24d914bb0f90c8e0d54d
Md. Hasibul Hasan <abeer.rocky@gmail.com>
2023-07-02 00:37:39 +0600

    Input validation done by express-validators

--- server/src/middlewares/uploadFile.js
+++ server/src/middlewares/uploadFile.js
@@ -3,7 +3,7 @@ const path = require('path');
 const createError = require('http-errors');
 
 
-const MAX_FILE_SIZE = Number(process.env.MAX_FILE_SIZE) || "2097152";
+const MAX_FILE_SIZE = Number(process.env.MAX_FILE_SIZE) || 2097152;
 const ALLOWED_FILE_TYPES = process.env.ALLOWED_FILE_TYPES || ['jpg', 'jpeg', 'png'];
 const UPLOAD_DIR = process.env.UPLOAD_IMAGES || 'public/images/users';
 
@@ -24,9 +24,9 @@ const storage = multer.diskStorage({
 const fileFilter = (req, file, cb) => {
     const extname = path.extname(file.originalname);
 
-    if (!ALLOWED_FILE_TYPES.includes(extname, substring(1))) {
+    if (!ALLOWED_FILE_TYPES.includes(extname.substring(1))) {
 
-        return cd(createError(400, 'file type not allowed'))
+        return cb(createError(400, 'file type not allowed'))
 
     }
     cb(null, true)
